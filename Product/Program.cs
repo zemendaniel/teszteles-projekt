2c87d2eb64bc6f21e497d5cad5303c4173715a5a
zemendaniel <zemendaniel@gmail.com>
2024-11-25 08:41:53 +0100

    discount

--- Product/Program.cs
+++ Product/Program.cs
@@ -20,30 +20,39 @@ namespace Product
             List<Product> list = new List<Product>();
             list.Add(new Product("Toothbrush", 100, home));
             list.Add(new Product("Hairdryer", 500, home));
-            list.Add(new Product("Pen", 5, office));
-            list.Add(new Product("Motherboard", 50000, computerParts));
+            list.Add(new Product("Pen", 5, office, 10));
+            list.Add(new Product("Motherboard", 50000, computerParts, 50));
             foreach (Product p in list)
             {
                 Console.WriteLine(p);
             }
 
             // list.Add(new Product("Huge Book", -100));
+            // list.Add(new Product("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", 100, home));
+            // list.Add(new Product("CPU", 1000000, computerParts, 200));
             Console.ReadKey();
         }
     }
     class Product
     {
         int price;
-        public Product(string name, int price, Category category)
+        string name;
+        // The discount is in percentage 
+        int discount;
+        public Product(string name, int price, Category category, int discount=0)
         {
             this.Name = name;
             // The price is in HUF!
             this.Price = price;
             Category = category;
+            this.Discount = discount;
         }
         public int Price
         {
-            get { return price; }
+            get
+            {
+                return price * (100 - discount) / 100;
+            }
             set
             {
                 if (value > 0)
@@ -56,7 +65,36 @@ namespace Product
                 }
             }
         }
-        public string Name { get; set; }
+        public int Discount
+        {
+            get { return discount; }
+            set
+            {
+                if (value >= 0 && value <= 100)
+                {
+                    discount = value;
+                }
+                else
+                {
+                    throw new Exception("Discount must be between 0 and 100.");
+                }
+            }
+        }
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value) && value.Length <= 32)
+                {
+                    name = value;
+                }
+                else
+                {
+                    throw new Exception($"Invalid value ({value}) for product name. It cannot be null or empty or longer than 32 characters.");
+                }
+            }
+        }
         public Category Category
         {
             get; set;
